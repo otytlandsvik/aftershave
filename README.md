870ac6b3b7cedad3d3848bff87f345eb02e349b5
Ole Tytlandsvik <ole.tytlandsvik@gmail.com>
2025-03-31 15:16:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Write compressed vectors to Zarr
+
+This small rust library is designed for specific use in a compression system for oceanographic simulations developed for Oceanbox.
---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+use zarrs::group::GroupBuilder;
+use zarrs::array::{ArrayBuilder, DataType, FillValue};
+use zarrs::storage::ReadableWritableListableStorage;
+use zarrs::filesystem::FileSystemStore;
+
+
