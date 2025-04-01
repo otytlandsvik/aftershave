a1d11cd87a0839d4e031fc7fb39518e45184cef2
Ole Tytlandsvik <ole.tytlandsvik@gmail.com>
2025-04-01 13:19:58 +0200

    devel: pin rust version

--- flake.nix
+++ flake.nix
@@ -5,27 +5,36 @@
       url = "github:numtide/flake-utils";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs =
     {
       self,
       nixpkgs,
       flake-utils,
+      rust-overlay,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        overlays = [
+          rust-overlay.overlays.default
+        ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        toolchain = pkgs.rust-bin.fromRustupToolchainFile ./toolchain.toml;
       in
       with pkgs;
       {
         devShells.default = mkShell {
           nativeBuildInputs = [
-            rustc
-            cargo
+            toolchain
           ];
-          RUST_BACKTRACE = 1;
+          RUST_BACKTRACE = "full";
+          RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
         };
       }
     );
